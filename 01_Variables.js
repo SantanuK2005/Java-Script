fda5d18bcfc56b7db39342727dfe8859affb6798
Santanu Khorat <santanukhorat@gmail.com>
2025-07-18 18:33:51 +0530

    Js

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+const accoundId = 1833809894
+let account_application ="ff" 
+var password = "1234567899"   //Mostly not used a var
+account_cuntry = "india"
+let accountstate   // it is undefined
+
+accountemail = "santanu.com"
+accountpassword = "S@1234"
+accountcity = "Kolkata"
+
+//console.log(accoundId,account_application,password,account_cuntry)
+console.table([accoundId, accountemail, accountpassword, accountcity])
---
+++ 02_DataType.js
@@ -0,0 +1,23 @@
+"Use strict";  // treat all js code as a newer version
+
+//alert(2+5)
+
+let name ="santanu"
+let age = 21
+let islogin = false
+
+/*  Data Type
+number =>2 to power 53
+bigint
+string=>""
+boolean => true or fales
+null  =>standalone value
+undefine => let name
+symbol => unique
+
+*/
+
+// Object
+
+console.log(typeof undefined);
+console.log(typeof null);
