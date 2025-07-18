d09aee51f5db92feffb2aa93a007d79748648af2
Santanu Khorat <santanukhorat@gmail.com>
2025-07-18 19:30:25 +0530

    Add initial JavaScript files for variables, data types, and a greeting message

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,24 @@
+let score = "50abc" 
+
+//console.log(typeof score) 
+//console.log(typeof(score))
+
+let valuueInNumber = Number(score)
+//console.log(typeof valuueInNumber)
+//console.log(valuueInNumber)  // NaN is a special Data type in JavaScript representing "Not a Number"
+
+
+// "33"=> 33
+//"33abc"=> NaN
+//true => 1; false => 0
+
+
+let isloggedIn = 1
+let booleanInloggedIn = Boolean(isloggedIn)
+//console.log(typeof booleanInloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); 
+console.log(typeof stringNumber); 
