e0918dcc48e5ea904c522afd6637ff35235809d8
Santanu Khorat <santanukhorat@gmail.com>
2025-07-19 20:28:01 +0530

    daly

--- 03_ConversionOperation.js
+++ 03_ConversionOperation.js
@@ -20,5 +20,43 @@ let booleanInloggedIn = Boolean(isloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber); 
+//console.log(stringNumber); 
+//console.log(typeof stringNumber); 
+
+//Next Start to Series 7
+
+let value = 3
+let nagValue = -value
+//console.log(nagValue); 
+
+//console.log(3 + 4); 
+//console.log(3 - 4); 
+//console.log(3 * 4);
+//console.log(3 / 4);
+//console.log(3**3);
+//console.log(3 % 4); // Modulus operator gives the remainder of the division
+
+let str1="hello"
+let str2="world"
+let str3= str1 + str2
+//console.log(str3); 
+
+
+//console.log("1" + 2); 
+//console.log(1+"2");
+//console.log("1"+2+2);  // String concatenation to the rules and guidelines in JavaScript
+//console.log(1+2+"2");  //@ECMAScript
+
+
+//console.log(true); // true and the (true+) not to a exist in js
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 //Not to prefer writing like this code
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // 101
+
+// Recomend to the reed a documentation im MDMN
---
+++ 04_comparision.js
@@ -0,0 +1,17 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+//console.log("2">1); // mostly avoid this type of comparison
+//console.log("02">1);
+//console.log(null>0);  //This conversion any time confusing.
+//console.log(null==0);
+//console.log(null>=0);
+
+
+// === Strct Equality Operator
+
+console.log("2"===2); // false
