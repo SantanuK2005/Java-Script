33fae1145219e613d400a88c88378cfddc1d5651
Santanu Khorat <santanukhorat@gmail.com>
2025-07-20 20:12:35 +0530

    string

---
+++ 01_Basic/05_string.js
@@ -0,0 +1,6 @@
+const name = "Santanu"
+const repoCount = 50
+
+// console.logn(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo cout is ${repoCount}`);  // (``) this symbol is Back text to used your code. and this is called Siring Interpolatins.
