590ac4ce80c3bd173a18c823f263eeb274b3e0ab
Santanu Khorat <santanukhorat@gmail.com>
2025-07-20 23:58:08 +0530

    number

--- 01_Basic/06_nums_and_math.js
+++ 01_Basic/06_nums_and_math.js
@@ -7,3 +7,8 @@ console.log(balancce);
 console.log(balancce.toString()); // Convert to string
 console.log(balancce.toFixed(2)); // Format to 2 decimal places
 
+const otherNumber = 123.45622;
+console.log(otherNumber.toPrecision(4)); // Format to 4 significant digits
+
+const longDigit = 1000000
+console.log(longDigit.toLocaleString('en-IN')); // Format with locale-specific separators
