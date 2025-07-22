8fb57cd457dc4a80f152cdb37fc8add7a4c335d4
Santanu Khorat <santanukhorat@gmail.com>
2025-07-23 00:08:50 +0530

    function

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,17 @@
+function saymyname() {
+    console.log("S")
+    console.log("A")
+    console.log("N")
+    console.log("T")
+    console.log("A") 
+    console.log("N")
+    console.log("U")   
+}
+
+// saymyname()
+
+function addTwoNumbers(num1, num2) {
+    console.log(num1 + num2);   
+}
+
+addTwoNumbers(5, 10);
