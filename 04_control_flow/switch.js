89e4bac60cd4440fce7bbb05b068bdea2ba51010
Santanu Khorat <santanukhorat@gmail.com>
2025-07-25 21:37:46 +0530

    truthy

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//  case value:
+   //     
+     //   break;
+
+   // default:
+    //    break;
+// }
+
+const month = 5  // naything are data type are chech is this switch.
+
+switch (month) {
+    case 1:
+        console.log("Januray");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;  // break is command then the all code are the default. its called Except default...
+    case 5:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Not match");
+        
+        break;
+}
