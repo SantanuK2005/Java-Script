083d9d931c36f4c399c30a3f5ea2aaad01627308
Santanu Khorat <santanukhorat@gmail.com>
2025-07-27 20:43:01 +0530

    for in/of

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(myObject[key]);
+    // console.log(`${key} shorcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp" ]
+for (const key in programming) {
+   console.log(programming[key]);
+   
+}
