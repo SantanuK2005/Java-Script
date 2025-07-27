d98007b672e95ab90e86dd30bde628660bf7b21f
Santanu Khorat <santanukhorat@gmail.com>
2025-07-27 21:49:21 +0530

    for in/of

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,40 +1,39 @@
-// for of
+const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// ["", "", ""]
-// [{}, {}, {}]
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
 
-const arr = [1, 2, 3, 4, 5]
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
 
-for (const num of arr) {
-    //console.log(num);
-}
-
-const greetings = "Hello world!"
-for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
-}
-
-// Maps
-
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
-
-
-// console.log(map);
-
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
-}
-
-const myObject = {
-    game1: 'NFS',
-    game2: 'Spiderman'
-}
-
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
+// function printMe(item){
+//     console.log(item);
 // }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
---
+++ 05_iterations/six.js
@@ -0,0 +1,26 @@
+// const coding = ["js", "rube", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (itme) => {
+//   console.log(itme);
+//    return itme
+// })
+//
+// console.log(values);
+
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+// const nweNums = mynums.filter( (num) => {
+//   return num > 4
+// })
+
+// const newNums = []
+
+// mynums.forEach( (num) => {
+//    if (num > 4) {
+//        newNums.push(num)
+//    }
+// })
+
+// console.log(newNums);
